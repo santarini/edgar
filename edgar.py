248cdfbca9a020994d197d193413103c1967e324
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-07 11:35:19 -1000

    Update edgar.py

--- edgar.py
+++ edgar.py
@@ -1,10 +1,18 @@
 #! python 3
+#from xbrl import XBRLParser, GAAP, GAAPSerializer
 import requests
+#from xml.etree import ElementTree
 import bs4 as bs
 
-
 response = requests.get('https://www.sec.gov/Archives/edgar/data/1652044/000165204418000027/goog-20180630.xml')
-soup = bs.BeautifulSoup(response.text, 'lxml')
-yearTable = soup.find('dei:DocumentFiscalPeriodFocus')
-print(
-# contextRef
+edgar_str = response.text
+
+print(edgar_str)
+
+
+##soup = bs.BeautifulSoup(response.text, 'lxml')
+##yearTable = soup.find('dei:DocumentFiscalPeriodFocus')
+##print(yearTable)
+### contextRef
+#xbrl_parser = XBRLParser()
+#xbrl = xbrl_parser.parse(file(response))
