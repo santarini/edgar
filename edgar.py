a6eabe3bf3092326f5f76c16e94c4293d7132b2c
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-07 11:05:09 -1000

    Update edgar.py

--- edgar.py
+++ edgar.py
@@ -1,8 +1,7 @@
-import os
+#! python 3
 import requests
 import bs4 as bs
-import csv
-import re
+
 
 response = requests.get('https://www.sec.gov/Archives/edgar/data/1652044/000165204418000027/goog-20180630.xml')
 soup = bs.BeautifulSoup(response.text, 'lxml')
