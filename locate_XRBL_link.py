ef04b388affc3607e6a2c016a895d09b80b2838e
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-07 15:25:01 -1000

    Update locate_XRBL_link.py

--- locate_XRBL_link.py
+++ locate_XRBL_link.py
@@ -1,34 +1,32 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 import sys
 
-# Access page
+# Company Data and Document Type
 cik = '0001652044'
 typeQuerry = '10-K'
 
 # Obtain HTML for search page
-base_url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type={}"
-edgar_resp = requests.get(base_url.format(cik, typeQuerry))
-edgar_str = edgar_resp.text
+baseURL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type={}"
+response = requests.get(baseURL.format(cik, typeQuerry))
+responseText = response.text
 
-# Find the document link
-doc_link = ''
-soup = BeautifulSoup(edgar_str, 'html.parser')
-table_tag = soup.find('table', class_='tableFile2')
-firstRow = table_tag.findAll('tr')[2]
+# Find filing detail link
+soup = bs(responseText, 'html.parser')
+table = soup.find('table', class_='tableFile2')
+firstRow = table.findAll('tr')[1]
 firstAnchor = firstRow.find('a')
 filingDetailLink = 'https://www.sec.gov' + firstAnchor.get("href")
 
 # Obtain HTML for document page
-doc_resp = requests.get(filingDetailLink)
-doc_str = doc_resp.text
-
+response = requests.get(filingDetailLink)
+responseText = response.text
 
 # Find the XBRL link
-soup = BeautifulSoup(doc_str, 'html.parser')
-table_tag = soup.find('table', class_='tableFile', summary='Data Files')
-firstRow = table_tag.find_all('tr')[1]
+soup = bs(responseText, 'html.parser')
+table = soup.find('table', class_='tableFile', summary='Data Files')
+firstRow = table.find_all('tr')[1]
 firstAnchor = firstRow.find('a')
-xbrl_link = 'https://www.sec.gov' + firstAnchor.get("href")
+XBRL_link = 'https://www.sec.gov' + firstAnchor.get("href")
 
-print(xbrl_link)
+print(XBRL_link)
