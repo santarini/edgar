a7d9efc4b099b3a3f7f1aac37e98c84ff568c096
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-07 14:47:12 -1000

    Update XRBL_parse.py

--- XRBL_parse.py
+++ XRBL_parse.py
@@ -13,3 +13,26 @@ for tag in tagList:
     if tag.name == 'us-gaap:stockholdersequity':
         print("Stockholder's equity: " + tag.text)
         print("Context Ref: " + str(tag.get('contextref')))
+
+###Income Statement Items
+
+#Revenues
+#if tag.name == 'us-gaap:Revenues'
+
+#Costs of Revenue
+##if tag.name == 'us-gaap:CostOfRevenue'
+
+#Total Costs and Expenses
+#if tag.name == 'us-gaap:CostsAndExpenses'
+
+
+
+        
+#if tag.name == 'us-gaap:OperatingIncomeLoss'
+
+#if tag.name == 'us-gaap:NonoperatingIncomeExpense'
+
+
+#if tag.name == 'us-gaap:NetIncomeLoss'
+
+
