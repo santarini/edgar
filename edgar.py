d37a5b3f3f7bd5c5df0aad3f56a6393df00b569c
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-07 14:17:06 -1000

    Update edgar.py

--- edgar.py
+++ edgar.py
@@ -3,12 +3,12 @@ import requests
 import sys
 
 # Obtain XBRL text from document
-xbrl_resp = requests.get('https://www.sec.gov/Archives/edgar/data/1652044/000165204418000027/goog-20180630.xml')
-xbrl_str = xbrl_resp.text
+response = requests.get('https://www.sec.gov/Archives/edgar/data/1652044/000165204418000027/goog-20180630.xml')
+responseText = response.text
 
 # Find and print stockholder's equity
-soup = BeautifulSoup(xbrl_str, 'lxml')
-tag_list = soup.find_all()
-for tag in tag_list:
+soup = BeautifulSoup(responseText, 'lxml')
+tagList = soup.find_all()
+for tag in tagList:
     if tag.name == 'us-gaap:stockholdersequity':
         print("Stockholder's equity: " + tag.text)
